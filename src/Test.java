5be1cf972d6c67e85aa2fecba6f06ccaafc5ba64
Tanirbergen <87113165+Tanirzum@users.noreply.github.com>
2021-10-07 15:44:41 +0600

    finall

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        OnTaskDoneListener listener1 = System.out::println;
+        OnTaskErrorListener listener2 = System.out::println;
+        Worker worker = new Worker(listener1, listener2);
+        worker.start();
+    }
+}
